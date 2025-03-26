27ccc59643788671ad24228516072b3505abae6a
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-03-26 17:43:39 +0530

    fix: refine layout in hero section by adjusting padding and margins for better visual alignment

---
+++ src/app/(pages)/privacy/page.tsx
@@ -0,0 +1,201 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="container pt-32 mx-auto px-4 max-w-6xl px-4">
+      <h1 className="text-3xl font-bold mb-6">M3labs Privacy Policy</h1>
+      <p className="text-sm text-gray-600 mb-8">Last updated on Mar 5th 2025</p>
+
+      <div className="space-y-6">
+        <section>
+          <p>
+            This privacy policy sets out how M3labs uses and protects any
+            information that you give M3labs when you visit their website and/or
+            agree to purchase from them.
+          </p>
+          <p className="mt-4">
+            M3labs is committed to ensuring that your privacy is protected.
+            Should we ask you to provide certain information by which you can be
+            identified when using this website, and then you can be assured that
+            it will only be used in accordance with this privacy statement.
+          </p>
+          <p className="mt-4">
+            M3labs may change this policy from time to time by updating this
+            page. You should check this page from time to time to ensure that
+            you adhere to these changes.
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">
+            Information We Collect
+          </h2>
+          <p>We may collect the following information:</p>
+          <ul className="list-disc pl-8 mt-2 space-y-1">
+            <li>Name</li>
+            <li>Contact information including email address</li>
+            <li>
+              Demographic information such as postcode, preferences and
+              interests, if required
+            </li>
+            <li>
+              Other information relevant to customer surveys and/or offers
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">
+            What We Do With the Information We Gather
+          </h2>
+          <p>
+            We require this information to understand your needs and provide you
+            with a better service, and in particular for the following reasons:
+          </p>
+          <ul className="list-disc pl-8 mt-2 space-y-1">
+            <li>Internal record keeping.</li>
+            <li>
+              We may use the information to improve our products and services.
+            </li>
+            <li>
+              We may periodically send promotional emails about new products,
+              special offers or other information which we think you may find
+              interesting using the email address which you have provided.
+            </li>
+            <li>
+              From time to time, we may also use your information to contact you
+              for market research purposes. We may contact you by email, phone,
+              fax or mail. We may use the information to customise the website
+              according to your interests.
+            </li>
+          </ul>
+          <p className="mt-4">
+            We are committed to ensuring that your information is secure. In
+            order to prevent unauthorised access or disclosure we have put in
+            suitable measures.
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">How We Use Cookies</h2>
+          <p>
+            A cookie is a small file which asks permission to be placed on your
+            computer's hard drive. Once you agree, the file is added and the
+            cookie helps analyze web traffic or lets you know when you visit a
+            particular site. Cookies allow web applications to respond to you as
+            an individual. The web application can tailor its operations to your
+            needs, likes and dislikes by gathering and remembering information
+            about your preferences.
+          </p>
+          <p className="mt-4">
+            We use traffic log cookies to identify which pages are being used.
+            This helps us analyze data about webpage traffic and improve our
+            website in order to tailor it to customer needs. We only use this
+            information for statistical analysis purposes and then the data is
+            removed from the system.
+          </p>
+          <p className="mt-4">
+            Overall, cookies help us provide you with a better website, by
+            enabling us to monitor which pages you find useful and which you do
+            not. A cookie in no way gives us access to your computer or any
+            information about you, other than the data you choose to share with
+            us.
+          </p>
+          <p className="mt-4">
+            You can choose to accept or decline cookies. Most web browsers
+            automatically accept cookies, but you can usually modify your
+            browser setting to decline cookies if you prefer. This may prevent
+            you from taking full advantage of the website.
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">
+            Controlling Your Personal Information
+          </h2>
+          <p>
+            You may choose to restrict the collection or use of your personal
+            information in the following ways:
+          </p>
+          <ul className="list-disc pl-8 mt-2 space-y-1">
+            <li>
+              Whenever you are asked to fill in a form on the website, look for
+              the box that you can click to indicate that you do not want the
+              information to be used by anybody for direct marketing purposes
+            </li>
+            <li>
+              If you have previously agreed to us using your personal
+              information for direct marketing purposes, you may change your
+              mind at any time by writing to or emailing us
+            </li>
+          </ul>
+          <p className="mt-4">
+            We will not sell, distribute or lease your personal information to
+            third parties unless we have your permission or are required by law
+            to do so. We may use your personal information to send you
+            promotional information about third parties which we think you may
+            find interesting if you tell us that you wish this to happen.
+          </p>
+          <p className="mt-4">
+            If you believe that any information we are holding on you is
+            incorrect or incomplete, please write to Plot No. 6b1, Block 70
+            shankar appa nagar, pimprala Jalgaon MAHARASHTRA 425001 . or contact
+            us at or as soon as possible. We will promptly correct any
+            information found to be incorrect.
+          </p>
+        </section>
+
+        <section className="mt-12">
+          <h1 className="text-3xl font-bold mb-6">
+            Cancellation & Refund Policy
+          </h1>
+          <p className="text-sm text-gray-600 mb-8">
+            Last updated on Mar 5th 2025
+          </p>
+
+          <p>
+            M3labs believes in helping its customers as far as possible, and has
+            therefore a liberal cancellation policy. Under this policy:
+          </p>
+          <ul className="list-disc pl-8 mt-4 space-y-2">
+            <li>
+              Cancellations will be considered only if the request is made
+              within 2-3 days of placing the order. However, the cancellation
+              request may not be entertained if the orders have been
+              communicated to the vendors/merchants and they have initiated the
+              process of shipping them.
+            </li>
+            <li>
+              M3labs does not accept cancellation requests for perishable items
+              like flowers, eatables etc. However, refund/replacement can be
+              made if the customer establishes that the quality of product
+              delivered is not good.
+            </li>
+            <li>
+              In case of receipt of damaged or defective items please report the
+              same to our Customer Service team. The request will, however, be
+              entertained once the merchant has checked and determined the same
+              at his own end. This should be reported within 2-3 days of receipt
+              of the products.
+            </li>
+            <li>
+              In case you feel that the product received is not as shown on the
+              site or as per your expectations, you must bring it to the notice
+              of our customer service within 2-3 days of receiving the product.
+              The Customer Service Team after looking into your complaint will
+              take an appropriate decision.
+            </li>
+            <li>
+              In case of complaints regarding products that come with a warranty
+              from manufacturers, please refer the issue to them.
+            </li>
+            <li>
+              In case of any Refunds approved by M3labs, it'll take 3-4 days for
+              the refund to be processed to the end customer.
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+}
